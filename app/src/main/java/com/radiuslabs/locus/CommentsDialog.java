35684619a036a06a23d7e9e72c11986731dd11f8
akshaybhasme <akshaybhasme91@gmail.com>
2016-04-30 18:25:54 -0700

    Comments layout and comments adapter added

---
+++ app/src/main/java/com/radiuslabs/locus/CommentsDialog.java
@@ -0,0 +1,17 @@
+package com.radiuslabs.locus;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CommentsDialog extends DialogFragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/radiuslabs/locus/adapters/CommentsAdapter.java
@@ -0,0 +1,69 @@
+package com.radiuslabs.locus.adapters;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.radiuslabs.locus.R;
+import com.radiuslabs.locus.models.Comment;
+
+import java.util.List;
+
+public class CommentsAdapter extends RecyclerView.Adapter<CommentsAdapter.ViewHolder> {
+
+    private List<Comment> comments;
+
+    private Context context;
+    private LayoutInflater inflater;
+
+    public CommentsAdapter(List<Comment> comments, Context context) {
+        this.comments = comments;
+        this.context = context;
+        inflater = LayoutInflater.from(context);
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = inflater.inflate(R.layout.row_interest, new LinearLayout(context));
+
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.tvText.setText(comments.get(position).getText());
+    }
+
+    @Override
+    public int getItemCount() {
+        return comments.size();
+    }
+
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView ivProfilePic;
+        TextView tvName;
+        TextView tvText;
+
+        public ViewHolder(View v) {
+            super(v);
+            ivProfilePic = (ImageView) v.findViewById(R.id.ivProfilePic);
+            tvName = (TextView) v.findViewById(R.id.tvName);
+            tvText = (TextView) v.findViewById(R.id.tvText);
+        }
+    }
+}
